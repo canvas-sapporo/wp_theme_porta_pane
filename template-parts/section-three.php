92221fed56385039896ab7aba30203967b617947
ALMERIA-25\yonezawamasahiro <kaihatuadmin@almeria.jp>
2026-01-21 14:40:01 +0900

    feat: add three section

--- template-parts/section-three.php
+++ template-parts/section-three.php
@@ -1,8 +1,48 @@
-<section id="three" class="px-24 py-12 bg-sub">
-  <h2 class="text-section-title">
-    ３つの魅力
-  </h2>
-  <p class="text-body">
-    テキストテキストテキスト
-  </p>
+<section id="three" class="flex flex-col items-center justify-center px-24 py-12 bg-sub">
+  <div class="relative flex items-end justify-center pb-9">
+    <div class="text-7xl font-bold text-bg tracking-widest select-none">
+      THREE
+    </div>
+    <h2 class="absolute flex items-center gap-2 text-text">
+      <img class=" h-9 w-9" src="<?php echo get_theme_image_url('tree-solid-full.svg'); ?>">
+      <span class="text-section-title tracking-tighter">
+        ３つの魅力
+      </span>
+    </h2>
+  </div>
+  <div class="flex w-full gap-9">
+    <div class="w-1/3">
+      <h3 class="text-center text-section-title">
+        国産小麦と自家製酵母
+      </h3>
+      <p class="text-center text-body">
+        国産小麦の風味を生かし、<br/>
+        自家製酵母でゆっくり発酵。<br/>
+        毎日食べても飽きのこない、<br/>
+        <span class="text-accent text-emphasis">やさしい味わい</span>です。
+      </p>
+    </div>
+    <div class="w-1/3">
+      <h3 class="text-center text-section-title">
+        焼きたての時間をお知らせ
+      </h3>
+      <p class="text-center text-body">
+        パンの焼き上がり時間を掲示し、<br/>
+        <span class="text-accent text-emphasis">一番おいしい瞬間</span>をお知らせしています。<br/>
+        できたての香りと温かさを<br/>
+        お楽しみください。
+      </p>
+    </div>
+    <div class="w-1/3">
+      <h3 class="text-center text-section-title">
+        特別すぎない、毎日の贅沢
+      </h3>
+      <p class="text-center text-body">
+        毎日の食卓に寄り添うパンだからこそ、<br/>
+        <span class="text-accent text-emphasis">背伸びしない美味しさ</span>を大切に。<br/>
+        日常の中で、ふっと気分が上がる存在でありたいと<br/>
+        考えています。
+      </p>
+    </div>
+  </div>
 </section>
