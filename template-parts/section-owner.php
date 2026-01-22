72a4a518a5884ccf8daabb90030ac08e412d0d04
ALMERIA-25\yonezawamasahiro <kaihatuadmin@almeria.jp>
2026-01-22 10:48:48 +0900

    feat: add owner section

--- template-parts/section-owner.php
+++ template-parts/section-owner.php
@@ -1,8 +1,41 @@
-<section id="owner" class="px-24 py-12">
-  <h2 class="text-section-title">
-    店主・お店の想い
-  </h2>
-  <p class="text-body">
-    テキストテキストテキスト
-  </p>
+<section id="owner" class="flex flex-col items-center justify-center px-24 py-12">
+  <div class="relative flex items-end justify-center pb-9">
+    <div class="text-7xl font-bold text-sub tracking-widest select-none">
+      OWNER
+    </div>
+    <h2 class="absolute flex items-center gap-2 text-text">
+      <img class=" h-9 w-9" src="<?php echo get_theme_image_url('tree-solid-full.svg'); ?>">
+      <span class="text-section-title tracking-tighter">
+        店主・お店の想い
+      </span>
+    </h2>
+  </div>
+  <div class="flex w-full gap-9">
+    <div class="w-1/3">
+      <img src="<?php echo get_theme_image_url('owner.png'); ?>">
+    </div>
+    <div class="w-2/3">
+      <h3 class="text-section-title">
+        店主紹介
+      </h3>
+      <h4 class="text-sub-title pt-4">
+        店主：山本 恒一（やまもと こういち）
+      </h4>
+      <p class="text-body pt-2">
+        製パン学校卒業後、都内のベーカリーやカフェで経験を積み、<br/>
+        「毎日食べたくなるパンを、自分の手で届けたい」という想いから Porta Pane をオープン。<br/>
+        素材選びと発酵の時間を大切にしながら、一つひとつ丁寧にパンを焼いています。
+      </p>
+      <h3 class="text-section-title pt-6">
+        店主メッセージ
+      </h3>
+      <p class="text-body pt-2">
+        Porta Paneのパンは、特別な日のためのものではありません。<br/>
+        朝食やランチ、仕事帰りのひとときなど、<br/>
+        日常の中で自然と手に取ってもらえる存在でありたいと考えています。<br/>
+        国産小麦や自家製酵母を使い、毎日食べても飽きない、やさしくて素直な味わいを目指しています。<br/>
+        この街の暮らしのそばに、そっと寄り添えるパン屋であれたら嬉しいです。
+      </p>
+    </div>
+  </div>
 </section>
