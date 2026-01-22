51df8e4c2e244fa8a3a6147aa505c8d032fac1ba
ALMERIA-25\yonezawamasahiro <kaihatuadmin@almeria.jp>
2026-01-22 10:58:54 +0900

    feat: add shop section

--- template-parts/section-shop.php
+++ template-parts/section-shop.php
@@ -1,8 +1,56 @@
-<section id="shop" class="px-24 py-12 bg-sub">
-  <h2 class="text-section-title">
-    お店について
-  </h2>
-  <p class="text-body">
-    テキストテキストテキスト
-  </p>
+<section id="shop" class="flex flex-col items-center justify-center px-24 py-12 bg-sub">
+  <div class="relative flex items-end justify-center pb-9">
+    <div class="text-7xl font-bold text-bg tracking-widest select-none">
+      SHOP
+    </div>
+    <h2 class="absolute flex items-center gap-2 text-text">
+      <img class=" h-9 w-9" src="<?php echo get_theme_image_url('tree-solid-full.svg'); ?>">
+      <span class="text-section-title tracking-tighter">
+        お店について
+      </span>
+    </h2>
+  </div>
+  <div class="flex w-full gap-9">
+    <div class="w-1/3">
+      <img src="<?php echo get_theme_image_url('map.png'); ?>">
+    </div>
+    <div class="w-1/3">
+      <h3 class="text-sub-title">
+        ご利用時間
+      </h3>
+      <p class="text-body">
+        10:00〜18:00<br/>
+        カフェのご利用は10:00〜17:00までとなります。
+      </p>
+      <h3 class="text-sub-title pt-4">
+        座席数
+      </h3>
+      <p class="text-body">
+        20席
+      </p>
+      <h3 class="text-sub-title pt-4">
+        駐車場
+      </h3>
+      <p class="text-body">
+        2台分あり
+      </p>
+    </div>
+    <div class="w-1/3">
+      <h3 class="text-sub-title">
+        住所
+      </h3>
+      <p class="text-body">
+        〒123-4567<br/>
+        北海道札幌市○○区△△町1-2-3　1F
+      </p>
+      <h3 class="text-sub-title pt-4">
+        アクセス
+      </h3>
+      <p class="text-body">
+        ○○駅南口より徒歩5分。<br/>
+        △△通りを直進し、<br/>
+        カフェ□□の角を曲がってすぐです。
+      </p>
+    </div>
+  </div>
 </section>
